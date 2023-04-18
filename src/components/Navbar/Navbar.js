f819f8076a0eb26d085a54f494aadd604f7bf8a4
Nafis-Abdelkarim <kazcrb@gmail.com>
2023-04-19 00:58:07 +0100

    Update my code

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
-import {MenuItem} from "./component/MenuItem"
+import {MenuItem} from "./MenuItem"
 import "./Navbar.css"
 
 class Navbar extends Component {
+    state = { clicked: false }
+
+    handelClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
     render() {
         return(
-            <nav className="NavbarItem">
-                <h1 className="navbar-logo">React <i className="fab fa-react"></i></h1>
-                <div className="menu-icon">
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1>
+                <div className="menu-icon" onClick={this.handelClick}>
+                    <i className={this.state.clicked ? 'fa fa-times':'fas fa-bars'}></i>
                 </div>
                 <ul>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}></ul>
                     {
                         MenuItem.map((item, index) => {
                             return(
